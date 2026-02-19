8171c0a7d367189bda1509f700573f9681a2a3e3
hasselmann007-dev <danilohasselmann007@gmail.com>
2026-02-18 23:10:06 -0300

    Add files via upload

--- OneDrive - BETA NEGÓCIOS/Apnovo/imobiliario_site/mcmv_site_fullstack/pages/admin.tsx
+++ OneDrive - BETA NEGÓCIOS/Apnovo/imobiliario_site/mcmv_site_fullstack/pages/admin.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const AdminClient = dynamic(() => import("../components/AdminClient"), {
+const AdminClient = dynamic(() => import("../components/AdminClient").then((m) => m.default), {
   ssr: false,
   loading: () => <div style={{ padding: 24 }}>Carregando painel…</div>,
 });
